c0d4290ae79effa8c9f73f2300bb3c39ef3b0bd4
sharly-dev <carlos.carrero@javeriana.edu.co>
2024-09-19 16:02:01 -0500

    >Enrrutamiento

--- Angular/ChillingDogsFrontend/src/app/app-routing.module.ts
+++ Angular/ChillingDogsFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./login/main/main.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path : 'login', component: MainComponent},
+  ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
