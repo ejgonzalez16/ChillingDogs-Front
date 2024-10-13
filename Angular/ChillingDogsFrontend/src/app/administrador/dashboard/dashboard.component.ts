e7f479b8e73db4f76387d5fb1a033b08c5e6dcee
DFallenKnight <168678995+DFallenKnight@users.noreply.github.com>
2024-10-12 23:14:45 -0500

    Actualizacion cada 10 segundos del dashboard (es cine)

--- Angular/ChillingDogsFrontend/src/app/administrador/dashboard/dashboard.component.ts
+++ Angular/ChillingDogsFrontend/src/app/administrador/dashboard/dashboard.component.ts
@@ -1,22 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { Administrador } from '../../modelo/administrador';
-import { Tratamiento } from '../../modelo/tratamiento';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { MedicamentosMes } from '../../modelo/medicamentosMes';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DashboardService } from '../../service/dashboard.service';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Subscription } from 'rxjs';
 
 @Component({
   selector: 'admin-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnDestroy {
   @Input()
   nombreAdministrador!: string;
   totalTratamientos!: number;
   totalTratamientosMes!: number;
-  medicamentosMes!: MedicamentosMes[]
+  medicamentosMes!: MedicamentosMes[];
   totalVetActivos!: number;
   totalVetInactivos!: number;
   totalMascotas!: number;
@@ -24,6 +22,7 @@ export class DashboardComponent {
   ventas!: number;
   ganancias!: number;
   topMedicamentos!: string[];
+  private intervalId!: any;
 
   constructor(private router: Router, private route: ActivatedRoute, private dashboardService: DashboardService) {}
 
@@ -31,6 +30,10 @@ export class DashboardComponent {
     this.route.params.subscribe(() => {
       this.obtenerDatosDashboard();
     });
+
+    this.intervalId = setInterval(() => {
+      this.obtenerDatosDashboard();
+    }, 10000);
   }
 
   obtenerDatosDashboard(): void {
@@ -73,5 +76,11 @@ export class DashboardComponent {
         console.error('Error al obtener los datos del dashboard', error);
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 }
