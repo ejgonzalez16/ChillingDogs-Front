3592092236f3ffb6aebac1eadf7f1df940bd293f
Édgar Julián González Sierra <edgarjuliangonzalezsierra167@gmail.com>
2024-10-28 19:05:27 -0500

    Front con los id para que el back le haga sus pruebas para el caso de uso 2 y correcion para caso de uso 1

---
+++ Angular/ChillingDogsFrontend/.angular/cache/18.2.4/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,25 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-PKW7FXYM.js";
+import "./chunk-IOOORW2T.js";
+import "./chunk-57WI53W5.js";
+import "./chunk-5TU6W3P2.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-GV3OJKON.js";
+import "./chunk-VDZEJD3D.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
---
+++ Angular/ChillingDogsFrontend/.angular/cache/18.2.4/vite/deps_ssr/primeng_api.js
@@ -0,0 +1,41 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeNGConfig,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+} from "./chunk-372AD3O2.js";
+import "./chunk-GV3OJKON.js";
+import "./chunk-VDZEJD3D.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeNGConfig,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+};
+//# sourceMappingURL=primeng_api.js.map
