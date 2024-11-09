7037414b6b37137be8d7b3fba99e7e1601a84f8e
DFallenKnight <168678995+DFallenKnight@users.noreply.github.com>
2024-11-08 20:31:58 -0500

    Filtro para vterinarios realizados

--- src/app/veterinario/search-bar-veterinario/search-bar-veterinario.component.ts
+++ src/app/veterinario/search-bar-veterinario/search-bar-veterinario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,14 +7,24 @@ import { Router } from '@angular/router';
   styleUrl: './search-bar-veterinario.component.scss'
 })
 export class SearchBarVeterinarioComponent {
-  cedulaVeterinario!: number;
+  nombreOrCedula!: string;
+  filtroEstado: string = "";
+
+  @Output() actualizarLista = new EventEmitter<{nombreOrCedula: string, filter: string}>();
 
   constructor(private router: Router) {}
 
   onSubmit() {
-    if (this.cedulaVeterinario) {
-      // Buscar cliente por cedula
-      this.router.navigate(['/veterinarios/buscar', this.cedulaVeterinario]);
-    }
+    console.log(this.filtroEstado)
+    // Buscar vet por cedula o nombre por nombre
+    this.actualizarLista.emit({
+      nombreOrCedula : this.nombreOrCedula,
+      filter : this.filtroEstado
+    });
+
+    // if (this.cedulaVeterinario) {
+    //   // Buscar cliente por cedula
+    //   this.router.navigate(['/veterinarios/buscar', this.cedulaVeterinario]);
+    // }
   }
 }
